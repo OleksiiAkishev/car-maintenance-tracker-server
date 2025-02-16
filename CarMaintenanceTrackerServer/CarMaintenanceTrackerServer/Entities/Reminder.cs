edc782ab9e3a40d740dd7a940d5d3a8cc63e9494
oleksii-akishev <QLB3@eurofins.com>
2025-02-16 16:51:30 +0100

    Created a skeleton for the Entites

---
+++ CarMaintenanceTrackerServer/CarMaintenanceTrackerServer/Entities/Reminder.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarMaintenanceTrackerServer.Entities
+{
+    public class Reminder
+    {
+        public int Id { get; set; }
+
+        public DateTime ReminderDate { get; set; }
+
+        public int Mileage { get; set; }
+
+        public string ServiceType { get; set; } = string.Empty;
+
+        public int CarId { get; set; }
+
+        [Required]
+        public required Car Car { get; set; }
+    }
+}
