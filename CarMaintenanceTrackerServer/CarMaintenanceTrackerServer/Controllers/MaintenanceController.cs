0260aeec68812aa20e7e123fecd70316ca450d6f
oleksii-akishev <QLB3@eurofins.com>
2025-02-16 17:09:05 +0100

    Introduced MaintenanceController and MaintenanceLogRequestDto, ReminderRequestDto

---
+++ CarMaintenanceTrackerServer/CarMaintenanceTrackerServer/Controllers/MaintenanceController.cs
@@ -0,0 +1,28 @@
+﻿using CarMaintenanceTrackerServer.DTOs.Maintenance;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarMaintenanceTrackerServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaintenanceController : ControllerBase
+    {
+        [HttpPost("log")]
+        public IActionResult LogMaintenance(MaintenanceLogRequestDto maintenanceLog)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{carId}")]
+        public IActionResult GetMaintenanceHistory(int carId)
+        {
+            return Ok();
+        }
+
+        [HttpPost("reminder")]
+        public IActionResult SetReminder(ReminderRequestDto reminder)
+        {
+            return Ok();
+        }
+    }
+}
