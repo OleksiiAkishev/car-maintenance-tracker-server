88c78e57dfe1b5302a8db53d691e7e42fe0797b2
oleksii-akishev <QLB3@eurofins.com>
2025-02-16 18:03:02 +0100

    Added CarMaintenanceTrackerDbContext and installed EntityFrameworkCore package

---
+++ CarMaintenanceTrackerServer/CarMaintenanceTrackerServer/Data/CarMaintenanceTrackerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CarMaintenanceTrackerServer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarMaintenanceTrackerServer.Data
+{
+    public class CarMaintenanceTrackerDbContext(DbContextOptions<CarMaintenanceTrackerDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<MaintenanceLog> MaintenanceLogs { get; set; }
+        public DbSet<Reminder> Reminders { get; set; }
+    }
+}
