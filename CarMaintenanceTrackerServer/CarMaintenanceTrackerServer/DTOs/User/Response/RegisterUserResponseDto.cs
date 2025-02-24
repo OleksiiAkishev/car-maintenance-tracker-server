f7f1c9a8f65f10596ef3296c5fd2c712873d48ef
oleksii-akishev <QLB3@eurofins.com>
2025-02-24 14:05:58 +0100

    Implemented UserMapper, no mapping now for GetUserResponse(Cars)

--- CarMaintenanceTrackerServer/CarMaintenanceTrackerServer/DTOs/User/Response/RegisterUserResponseDto.cs
+++ CarMaintenanceTrackerServer/CarMaintenanceTrackerServer/DTOs/User/Response/RegisterUserResponseDto.cs
@@ -2,7 +2,7 @@
 {
     public class RegisterUserResponseDto
     {
-        public int UserId { get; set; }
+        public int Id { get; set; }
         public string? Username { get; set; }
         public string? Email { get; set; }
     }
